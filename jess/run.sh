59a0283a961d6bafff2fb712d0054bdec71f1046
Jess Woods <woodsjk@seas.upenn.edu>
2021-12-06 19:09:56 -0500

    notes

--- jess/run.sh
+++ jess/run.sh
@@ -2,3 +2,7 @@
 cargo build --release --example circ_c
 RUST_LOG=debug RUST_BACKTRACE=1 ./target/release/examples/circ_c jess/add.c
 
+
+RUST_LOG=debug RUST_BACKTRACE=1 ./target/release/examples/circ ./examples/ZoKrates/mpc/unit_tests/arithmetic_tests/2pc_add.zok r1cs --action count
+
+
